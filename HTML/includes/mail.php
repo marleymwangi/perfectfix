2446f8643c8f397c0bc914bd34944b880a38e7c0
marleymwangi <bobmwangee@gmail.com>
2017-09-30 02:11:42 +0300

    mail

--- HTML/includes/mail.php
+++ HTML/includes/mail.php
@@ -1,5 +1,31 @@
 <?php
-
-phpinfo()
+/* try to connect */
+$inbox = imap_open('{imappro.zoho.com:993/imap/ssl/novalidate-cert} /Inbox','trans@perfectfix.co','perfectfix') or die('Cannot connect to perfectfix domain');
+/* grab emails */
+$emails = imap_search($inbox,'ALL');
+/* if emails are returned, cycle through each... */
+if($emails) {        
+        /* begin output var */
+        $output = '';        
+        /* put the newest emails on top */
+        rsort($emails);        
+        /* for every email... */
+        foreach($emails as $email_number) {                
+                /* get information specific to this email */
+                $overview = imap_fetch_overview($inbox,$email_number,0);
+                $message = imap_fetchbody($inbox,$email_number,2);                
+                /* output the email header information */
+                $output.= '<div class="toggler '.($overview[0]->seen ? 'read' : 'unread').'">';
+                $output.= '<span class="subject">'.$overview[0]->subject.'</span> ';
+                $output.= '<span class="from">'.$overview[0]->from.'</span>';
+                $output.= '<span class="date">on '.$overview[0]->date.'</span>';
+                $output.= '</div>';
+                
+                /* output the email body */
+                $output.= '<div class="body">'.$message.'</div>';
+        }        
+        echo $output;
+}
+imap_close($inbox);
 
 ?>
