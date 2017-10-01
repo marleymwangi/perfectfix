26c9228c5f40d4ff023e5c9cd5020d2b63eb03bf
marleymwangi <bobmwangee@gmail.com>
2017-10-01 17:55:38 +0300

    corrections

--- HTML/layouts/menumain.php
+++ HTML/layouts/menumain.php
@@ -2,6 +2,5 @@
     <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider <?php if ($page=='home') {echo'-is-active'; }?>" href="index.php">Home</a></li>
     <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider <?php if ($page=='about') {echo'-is-active'; }?>"" href="about.php">About</a></li>
     <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider <?php if ($page=='services') {echo'-is-active'; }?>" href="services.php">Services</a></li>
-    <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider <?php if ($page=='faq') {echo'-is-active'; }?>" href="faq.php">FAQ</a></li>
     <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider <?php if ($page=='contacts') {echo'-is-active'; }?>" href="contacts.php">Contacts</a></li>
 </ul>
