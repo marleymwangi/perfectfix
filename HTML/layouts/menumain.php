d4b2504643905cb74c8b2489b9447a8f33c6434d
marleymwangi <bobmwangee@gmail.com>
2017-10-05 02:29:27 +0300

    corrections

--- HTML/layouts/menumain.php
+++ HTML/layouts/menumain.php
@@ -1,6 +1,6 @@
 <ul class="list-unstyled s-header__nav-menu">
     <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider <?php if ($page=='home') {echo'-is-active'; }?>" href="index.php">Home</a></li>
     <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider <?php if ($page=='about') {echo'-is-active'; }?>"" href="about.php">About</a></li>
-    <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider <?php if ($page=='services') {echo'-is-active'; }?>" href="services.php">Services</a></li>
+    <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider <?php if ($page=='services') {echo'-is-active'; }?>" href="services.php">Packages</a></li>
     <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider <?php if ($page=='contacts') {echo'-is-active'; }?>" href="contacts.php">Contacts</a></li>
 </ul>
