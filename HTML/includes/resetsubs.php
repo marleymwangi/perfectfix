f3349b1f8016ef5d4fd2b9c3d856de321560252a
marleymwangi <bobmwangee@gmail.com>
2017-10-10 01:59:39 +0300

    corrections

--- HTML/includes/resetsubs.php
+++ HTML/includes/resetsubs.php
@@ -3,14 +3,26 @@
 
 include_once 'dbh.inc.php';
 
-$sql = "DELETE * FROM subs;";
+$sql = "DELETE FROM subs;";
 mysqli_query($conn,$sql);
+if (!mysqli_query($con,$sql))
+  {
+  echo("Error description: " . mysqli_error($con));
+  }
 
 
 $sql = "ALTER TABLE subs AUTO_INCREMENT = 1;";
 mysqli_query($conn,$sql);
+if (!mysqli_query($con,$sql))
+  {
+  echo("Error description: " . mysqli_error($con));
+  }
 
 $sql = "INSERT INTO subs (userId, package1, package2, package3, package4) VALUES (1,1,1,1,1);";
 mysqli_query($conn,$sql);
+if (!mysqli_query($con,$sql))
+  {
+  echo("Error description: " . mysqli_error($con));
+  }
 
 ?>
