b59988c1ab484c698ff65f6fd29c9b0366c13fdf
marleymwangi <bobmwangee@gmail.com>
2017-10-10 01:10:51 +0300

    corrections

--- HTML/test.php
+++ HTML/test.php
@@ -1,14 +1,5 @@
 <?php 
 
-session_start();
-include_once 'includes/dbh.inc.php';
-
-if (isset($_POST['submit'])) {
-	$userName = mysqli_real_escape_string($conn, $_POST['userName']);
-	$passwrd = mysqli_real_escape_string($conn, $_POST['passwrd']);
-
-	echo $userName.$passwrd;
-
-}
+phpinfo()
 
 ?>
