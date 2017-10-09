2cbcc9738ba60c9097e40a296f49fc46eeb75952
marleymwangi <bobmwangee@gmail.com>
2017-10-09 22:36:34 +0300

    corrections

--- HTML/includes/resetsubs.php
+++ HTML/includes/resetsubs.php
@@ -9,4 +9,7 @@ mysqli_query($conn,$sql);
 $sql = "ALTER TABLE subs AUTO_INCREMENT = 1;";
 mysqli_query($conn,$sql);
 
+$sql = "INSERT INTO subs (userId, package1, package2, package3, package4) VALUES (1,1,1,1,1);";
+mysqli_query($conn,$sql);
+
 ?>
