41d8a67d68a293ac1ee8803da179828be662b01c
marleymwangi <bobmwangee@gmail.com>
2017-11-04 23:58:47 +0300

    corrections

--- HTML/includes/payments/receivePayments.php
+++ HTML/includes/payments/receivePayments.php
@@ -1,6 +1,6 @@
 <?php
 
-$data  = json_decode(file_get_contents('php://input');
+$data  = json_decode(file_get_contents('php://input'));
 print_r($data);
 // Process the data...
 $category = $data["category"];
