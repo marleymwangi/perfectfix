2f8956306fa0186b9fa9383f8b44c832413335ad
marleymwangi <bobmwangee@gmail.com>
2017-11-04 23:53:05 +0300

    corrections

--- HTML/includes/payments/receivePayments.php
+++ HTML/includes/payments/receivePayments.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once '../dbh.inc.php';
-
 $data  = json_decode(file_get_contents('php://input'), true);
 print_r($data);
 // Process the data...
@@ -11,9 +9,6 @@ if ( $category == "MobileC2B" ) {
    $phoneNumber = $data["source"];
    $value       = $data["value"];
    $account     = $data["clientAccount"];
-
-   $sql = "INSERT INTO test (phoneNo, value, account) VALUES ('$phoneNumber', '$value', '$account');";
-	mysqli_query($conn, $sql);
 
 
    // manipulate the data as required by your business logic
